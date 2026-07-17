9474386b911a61342c366121283b63b6405a0ce6
Riyaarora069 <riarora7316@gmail.com>
2026-07-18 01:17:00 +0530

    Maximum sum of subarray(Brute force approach)

--- printsubarray.cpp
+++ printsubarray.cpp
@@ -1,16 +1,37 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 void printsubarray(int arr[],int size){
+    int sum=0;
+    int maxsum=INT_MIN;
     for(int start=0;start<size;start++){
         for(int end=start;end<size;end++){
-        cout<<"["<<start<<","<<" "<<end<<"]";
+        //cout<<"["<<start<<","<<" "<<end<<"]";
+        int sum=0;
+        
+        for(int i=start;i<=end;i++){
+            //cout<<arr[i];
+            sum=sum+arr[i];
+            
+        
+        
+
+
+        }
+        cout<<sum<<",";
+        maxsum=max(sum,maxsum);
+
+        
 
     }
     cout<<endl;
 }
+cout<<"The maximum sum of subarray ="<<maxsum<<endl;
 }
+
 int main(){
     int arr[5]={1,2,3,4,5};
     int n=5;
     printsubarray(arr,n);
+    
 }
